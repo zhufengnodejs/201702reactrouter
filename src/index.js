9a50d554c8608d39d8d9bc779f0881a48b422e28
zhufengnodejs <zhufengnodejs@126.com>
2017-06-14 15:26:15 +0800

    1.redux+jquery

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import {createStore} from './redux';
 import $ from 'jquery';
 //就是管理员函数，
 //state是旧的状态对象，action是动作对象 {type:'INCREMENT'}
-let reducer = (state=0,action={})=>{
+let reducer = (state={number:0},action={})=>{
     switch (action.type){
         case 'ADD'://加1
-            return state+1;
+            return {number:state.number+1};
         case 'SUB'://减1
-            return state - 1;
+            return {number:state.number-1};
         default:
             return state;
     }
@@ -22,15 +22,21 @@ $(document.body).append(`
   <button id="subBtn">-</button>
 </div>
 `);
-store.subscribe(()=>{
-    $('#counter').html(store.getState())
-})
-
+let render = ()=>$('#counter').html(store.getState().number);
+store.subscribe(render);
+render();
+//简化调用 不容易出错 ActionCreator
+let addNum = ()=>{
+    return {type:'ADD'};
+}
+let subNum = ()=>{
+    return {type:'SUB'};
+}
 $('#addBtn').click(()=>{
-    store.dispatch({type:'ADD'});
+    store.dispatch(addNum());
 });
 $('#subBtn').click(()=>{
-    store.dispatch({type:'SUB'});
+    store.dispatch(subNum());
 });
 
 
--- src/redux.js
+++ src/redux.js
@@ -1,3 +1,4 @@
+//创建一个仓库
 let createStore = (reducer)=>{
     let state;//先定义一个默认值
     let listeners = [];//存放着所有的监听函数
@@ -7,15 +8,16 @@ let createStore = (reducer)=>{
         listeners.push(listener);
         //调用subscribe的返回值会取消掉此函数的监听
         return ()=>{
-            listeners = listeners.filter(l=>l!=listener);
+            listeners = listeners.filter(l=>l!=listener);//调用此函数表示取消订阅
         }
     }
     //派发action
-    let dispatch = (action)=>{
+    let dispatch = (action)=>{//{type:'ADD'}
         //把老的状态和action传入reducer,返回新的状态
         state = reducer(state,action);
         listeners.forEach(l=>l());
     }
+    dispatch();
     return {
         getState,//获取最新的状态
         subscribe,//订阅状态变化事件
