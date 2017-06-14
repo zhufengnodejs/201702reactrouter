569fdb78e5c8df2236d5f6e8cfeca86eec0647f4
zhufengnodejs <zhufengnodejs@126.com>
2017-06-14 16:14:57 +0800

    1.redux+jquery

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import {createStore} from './redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
 class Todos extends React.Component {
+    constructor(){
+        super();
+        this.state = {todos:[{id:Date.now(),content:'学习React',completed:false}]};
+    }
     render() {
         return (
             <div>
                 <input type="text"/>
                 <ul>
-
+                    {
+                        this.state.todos.map(todo=><li>{todo.content}</li>)
+                    }
                 </ul>
             </div>
         )
