0375e1afca9a5e927667af23bc294cc78c9ba8a6
zhufengnodejs <zhufengnodejs@126.com>
2017-06-14 12:49:11 +0800

    redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-ReactDOM.render(
-    App,
-    document.querySelector('#root')
-);
+import {createStore} from './redux';
+//就是管理员函数，
+//state是旧的状态对象，action是动作对象 {type:'INCREMENT'}
+let reducer = (state=0,action={})=>{
+    switch (action.type){
+        case 'INCREMENT'://加1
+            return state+1;
+        case 'DECREMENT'://减1
+            return state - 1;
+        default:
+            return state;
+    }
+}
+let store = createStore(reducer);
+
---
+++ src/redux.js
@@ -0,0 +1,25 @@
+let createStore = (reducer)=>{
+    let state;//先定义一个默认值
+    let listeners = [];//存放着所有的监听函数
+    let getState = ()=>state;
+    //订阅状态变化事件，当状态发生变化的时候会调用listener函数
+    let subscribe = listener=>{
+        listeners.push(listener);
+        //调用subscribe的返回值会取消掉此函数的监听
+        return ()=>{
+            listeners = listeners.filter(l=>l!=listener);
+        }
+    }
+    //派发action
+    let dispatch = (action)=>{
+        //把老的状态和action传入reducer,返回新的状态
+        state = reducer(state,action);
+        listeners.forEach(l=>l());
+    }
+    return {
+        getState,//获取最新的状态
+        subscribe,//订阅状态变化事件
+        dispatch //派发action 改变状态 使监听函数执行
+    }
+}
+export {createStore};
