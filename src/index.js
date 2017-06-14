c0de5ef6eb742c32b7cffb65f31bb2ad4c980eb9
zhufengnodejs <zhufengnodejs@126.com>
2017-06-14 16:27:48 +0800

    1.redux+jquery

--- src/index.js
+++ src/index.js
@@ -4,15 +4,29 @@ import ReactDOM from 'react-dom';
 class Todos extends React.Component {
     constructor(){
         super();
-        this.state = {todos:[{id:Date.now(),content:'学习React',completed:false}]};
+        this.state = {todos:[]};
+    }
+    handleKeyDown = (event)=>{
+        if(event.keyCode == 13 ){
+            let content = event.target.value;
+            this.state.todos.push({
+                id:Date.now(), content, completed:false
+            });
+            this.setState({todos:this.state.todos});
+            event.target.value = '';
+        }
+    }
+    handleClick = (id)=>{
+        let todos = this.state.todos.filter(todo=>todo.id != id);
+        this.setState({todos});
     }
     render() {
         return (
             <div>
-                <input type="text"/>
+                <input type="text" onKeyDown={this.handleKeyDown}/>
                 <ul>
                     {
-                        this.state.todos.map(todo=><li>{todo.content}</li>)
+                        this.state.todos.map(todo=><li>{todo.content} <button onClick={()=>this.handleClick(todo.id)}>-</button></li>)
                     }
                 </ul>
             </div>
